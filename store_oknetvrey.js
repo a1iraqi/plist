2d7913494056bd96b371d23806643d5f47ce2678
a1iraqi <abood.fi123@gmail.com>
2017-07-06 23:46:39 +0300

    js

---
+++ store_oknetvrey.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+
+    var copy=document.getElementById("a1iraqi");
+    
+    if(copy==null){
+        window.location.href="http://www.pro1iq.com/";
+    };
+    copy.setAttribute("href","http://www.pro1iq.com/");
+  copy.innerHTML="تصميم : برو عراقي"
+};
